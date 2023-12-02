a1166fb561fedaedccbb62a06fa1740677d4089c
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-02 16:02:13 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-terraform {
-  backend "s3" {
-  }
-}
-variable "test" {
-}
-output "test" {
-  value = var.test
+module "components" {
+
+  source                 = "git::https://github.com/sivalingam2/remote_module.git"
+  for_each = var.components
+  ami                    = var.ami
+  instance_type          = each.value["instance_type"]
+  vpc_security_group_ids = var.vpc_security_group_ids
+  zone_id                = var.zone_id
+  name                   = each.value["name"]
 }
---
+++ varibale.tf
@@ -0,0 +1,4 @@
+variable "components" {}
+variable "zone_id" {}
+variable "vpc_security_group_ids" {}
+variable "ami" {}
