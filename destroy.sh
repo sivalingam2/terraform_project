b104bc42a2ce870dd378b035cd3fceebeca467a8
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-02 16:15:32 +0530

    terraform

--- destroy.sh
+++ destroy.sh
@@ -1,6 +1,6 @@
 prod_destroy () {
-  rm -rf .terraform
-  terraform init -backend-config=env-prod/state.tfvars
-  terraform destroy -auto-approve -var-file=env-prod/main.tfvars
+   rm -rf .terraform
+    terraform init -backend-config=env-dev/state.tfvars
+    terraform destroy -auto-approve -var-file=env-dev/main.tfvars
 }
 prod_destroy
