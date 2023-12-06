5c5a170308fcadb7b5bf90f3863205c5c89a1080
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-06 18:58:27 +0530

    create vpc

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "components" {
-  source                 = "./tf-vpc-module"
+  source                 = "git::https://github.com/sivalingma2/tf-vpc-module"
   for_each = var.vpc
   cidr = each.value["cidr"]
 
